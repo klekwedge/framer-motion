f1506eefa927f6741740fd0e8dd413cb550f46a8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-11 15:22:55 +0300

    feat: add all files for framer motion

---
+++ 1. Framer Motion/02 ShowingHiding Elements/src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  { category: "cars", title: "BMW" },
+  { category: "cars", title: "Volvo" },
+  { category: "cars", title: "Mustang" },
+  { category: "colors", title: "Blue" },
+  { category: "colors", title: "Orange" },
+  { category: "colors", title: "Red" },
+  { category: "animals", title: "Cat" },
+  { category: "animals", title: "Dog" },
+  { category: "animals", title: "Cow" },
+  { category: "fruits", title: "Melon" },
+  { category: "fruits", title: "Kiwi" },
+  { category: "fruits", title: "Banana" },
+];
---
+++ 1. Framer Motion/03 AnimateSharedLayout/src/components/Filter.js
@@ -0,0 +1,78 @@
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { useState } from "react";
+import Button from "./Button";
+
+const Filter = ({ data }) => {
+  const [cards, setCards] = useState(data);
+  const [selected, setSelected] = useState(0);
+
+  const buttons = data.reduce(
+    (acc, el) => {
+      if (acc.includes(el.category)) return acc;
+      return [...acc, el.category];
+    },
+    ["all"]
+  );
+
+  const handleFilter = (selector) => {
+    if (selector === "all") return setCards(data);
+    setCards(data.filter((el) => el.category === selector));
+  };
+
+  return (
+    <AnimateSharedLayout>
+      <motion.div layout>
+        <motion.div style={{ background: "#f1f1f1" }}>
+          {buttons.map((btn, index) => (
+            <Button
+              key={btn}
+              text={btn}
+              handleClick={() => {
+                handleFilter(btn);
+                setSelected(index);
+              }}
+              isSelected={selected === index}
+            ></Button>
+          ))}
+        </motion.div>
+        <motion.div
+          layout
+          style={{
+            overflow: "hidden",
+            maxWidth: 312,
+            border: "1px solid #ccc",
+          }}
+        >
+          <AnimatePresence initial={false} exitBeforeEnter>
+            {cards.map((el) => (
+              <motion.div
+                layout
+                key={el.title}
+                style={boxStyle}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+              >
+                {console.log(el.title)}
+                {el.title}
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </motion.div>
+      </motion.div>
+    </AnimateSharedLayout>
+  );
+};
+
+const boxStyle = {
+  float: "left",
+  backgroundColor: "#2196F3",
+  color: "#ffffff",
+  width: "100px",
+  lineHeight: "100px",
+  textAlign: "center",
+  margin: "2px",
+};
+
+export default Filter;
